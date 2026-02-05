d896f6df3ef3ca1cec3c33acebfdba981a36be0a
codebyazeredo <matheus.azeredo.pro@gmail.com>
2026-02-04 22:03:17 -0300

    refactor: improve formatting and readability of NotificationController

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,21 +1,23 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { CreateNotificationDto } from './dto/create-notification.dto';
 import { NotificationService } from './notification.service';
 import { randomUUID } from 'crypto';
 
 @Controller('notifications')
 export class NotificationController {
-    constructor(private readonly notificationService: NotificationService) {}
+  constructor(
+    private readonly notificationService: NotificationService,
+  ) {}
 
-    @Post
-    async create(@Body() dto: CreateNotificationDto) {
-        await this.notificationService.enqueue({
-            ...dto,
-            correlationId: randomUUID(),
-        });
+  @Post()
+  async create(@Body() dto: CreateNotificationDto) {
+    await this.notificationService.enqueue({
+      ...dto,
+      correlationId: randomUUID(),
+    });
 
-        return {
-            status: 'Adicionado a fila de envio com sucesso',
-        }
-    }
+    return {
+      status: 'Adicionado à fila de envio com sucesso',
+    };
+  }
 }
