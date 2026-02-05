1cc4f0558ad37411092c8da2ba7cbffdcae540c8
codebyazeredo <matheus.azeredo.pro@gmail.com>
2026-02-04 21:52:02 -0300

    feat: implement enqueue method in NotificationService for job processing

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectQueue } from '@nestjs/bullmq';
+import { Queue } from 'bullmq';
+import { NotificationJob } from './interfaces/notification-job.interface';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+    constructor(@InjectQueue('notifications') private readonly notificationQueue: Queue) {}
+
+     async enqueue(job: NotificationJob) {
+        await this.notificationQueue.add('send-notification', job, {
+            attempts: 5,
+            backoff: {
+                type: 'exponential',
+                delay: 3000,
+            },
+            removeOnComplete: true,
+            removeOnFail: false,
+        });
+     }
+}
