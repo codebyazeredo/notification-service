39d702cfc0d1e902091eb421460a65996a7bdc88
codebyazeredo <matheus.azeredo.pro@gmail.com>
2026-02-04 21:59:38 -0300

    feat: add QueueModule for managing notification job queues

---
+++ src/queue/queue.module.ts
@@ -0,0 +1,18 @@
+import { BullModule } from '@nestjs/bullmq';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [
+    BullModule.forRoot({
+      connection: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'notifications',
+    }),
+  ],
+  exports: [BullModule],
+})
+export class QueueModule {}
