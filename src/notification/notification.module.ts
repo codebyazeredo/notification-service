4f7733572de0125399f72a1ee8adcb45b7dc040a
codebyazeredo <matheus.azeredo.pro@gmail.com>
2026-02-04 21:59:54 -0300

    feat: add NotificationProcessor, EmailChannel, and TemplateService to NotificationModule providers

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
+import { NotificationProcessor } from './notification.processor';
+import { EmailChannel } from './channels/email.channel';
+import { TemplateService } from './template.service';
 
 @Module({
   controllers: [NotificationController],
-  providers: [NotificationService]
+  providers: [
+    NotificationService,
+    NotificationProcessor,
+    EmailChannel,
+    TemplateService,
+  ],
 })
 export class NotificationModule {}
