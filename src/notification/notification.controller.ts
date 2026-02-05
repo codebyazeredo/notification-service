6c6566b1df8a8564f5524b5caba33bb83ce17c22
codebyazeredo <matheus.azeredo.pro@gmail.com>
2026-02-04 21:49:05 -0300

    feat: implement create notification endpoint in NotificationController

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { CreateNotificationDto } from './dto/create-notification.dto';
+import { NotificationService } from './notification.service';
+import { randomUUID } from 'crypto';
 
 @Controller('notification')
-export class NotificationController {}
+export class NotificationController {
+    constructor(private readonly notificationService: NotificationService) {}
+
+    @Post
+    async create(@Body() dto: CreateNotificationDto) {
+        await this.notificationService.enqueue({
+            ...dto,
+            correlationId: randomUUID(),
+        });
+
+        return {
+            status: 'Adicionado a fila de envio com sucesso',
+        }
+    }
+}
